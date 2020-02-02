7a67751b3f8a78bb8b8f2ea379965dfacb2648c3
Daniel Rossmeisl <42306528+drossmei@users.noreply.github.com>
2020-02-01 21:13:59 -0800

    GUI Version Start

--- test/openCV.py
+++ test/openCV.py
@@ -1,48 +1,63 @@
 import cv2
 from functions import *
-from pickMeme import chooseMeme
+from pickMeme import chooseMeme, data
 
-# Initialize Camera
-cam = cv2.VideoCapture(0)
-
-# load images
-glasses_img = cv2.imread('test/images/glasses2.png', -1)
-crying_img = cv2.imread('test/images/cryingeyes.png', -1)
-redeye_img = cv2.imread('test/images/redeye.png', -1)
-imgGlassesGray = cv2.cvtColor(glasses_img, cv2.COLOR_BGR2GRAY)
-face_cascade = cv2.CascadeClassifier('test/data/haarcascade_frontalface_alt2.xml')
-
-# find emotion and caption
-emotion = chooseMeme()[0]
-caption = chooseMeme()[1]
-
-while True:
-    ret, frame = cam.read()
-    store_frame = frame.copy()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(frame)
-
-    # width and height of image
-    width  = cam.get(3)
-    height = cam.get(4)
-
-    # Apply Emotion Text
-    textOverlay(frame, (f"Make a {emotion} face", "Press space to submit"), width, height)
     
-    cv2.imshow("MemeMe", frame)
-    
-    if not ret:
-        break
-    k = cv2.waitKey(1)
-    
-    if k%256 == 32:
-            # SPACE pressed
+def main():
+    # Initialize Camera
+    cam = cv2.VideoCapture(0)
+
+    # load images
+    glasses_img = cv2.imread('test/images/glasses2.png', -1)
+    crying_img = cv2.imread('test/images/cryingeyes.png', -1)
+    redeye_img = cv2.imread('test/images/redeye.png', -1)
+    imgGlassesGray = cv2.cvtColor(glasses_img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier('test/data/haarcascade_frontalface_alt2.xml')
+
+    # find emotion and caption and image filter
+    temp = chooseMeme()
+    emotion = temp[0]
+    caption = temp[1]
+
+    def applyFilter(store_frame, faces, emotion):
+        if (emotion == "happy"):
+            pass
+        if (emotion == "sad"):
+            crying(store_frame, faces, crying_img)
+        elif (emotion == "surpised"):
+            pass
+        elif (emotion == "badass"):
             glasses(store_frame, faces, glasses_img)
+
+
+    while True:
+        ret, frame = cam.read()
+        store_frame = frame.copy()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(frame)
+
+        # width and height of image
+        width  = cam.get(3)
+        height = cam.get(4)
+
+        # Apply Emotion Text
+        textOverlay(frame, (f"Make a {emotion} face", "Press space to submit"), width, height)
+        
+        cv2.imshow("MemeMe", frame)
+        
+        if not ret:
+            break
+        k = cv2.waitKey(1)
+        
+        if k%256 == 32:
+            # SPACE pressed
+            applyFilter(store_frame, faces, emotion)
             textOverlay(store_frame, (caption,""), width, height)
             cv2.imwrite("MemeMe'd.png", store_frame)
             print("Saving Image...")
             break
 
 
-cam.release()
-cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
+    openImage()
---
+++ test/runGUI.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import Tk, BOTTOM, messagebox
+from tkinter.ttk import Label, Combobox, Button
+from openCV import main, data
+
+# initialize tkinter
+root = Tk()
+root.title("MemeMe")
+
+emotions = data.keys()
+
+def clicked():
+    main()
+
+#Intialize Title Label
+titleLabel = Label(root, text="MemeMe", font=("Courier", 44))
+titleLabel.pack()
+
+
+#Initialize Weather Label
+emotionLabel = Label(root, text="Enter Custom: ", font=("Courier", 20))
+emotionLabel.pack(pady=10)
+
+
+#Initialize Drop Down Menu
+memeCombo = Combobox(root)
+memeCombo['values'] = sorted(c for c in emotions)
+memeCombo.current(0)
+memeCombo.pack(pady=10)
+
+
+#Initialize Weather Button 
+memeBtn = Button(root, text="Memeify", width=20, command=clicked)
+memeBtn.pack(pady=10)
+
+
+#Initialize Exit Button
+exitBtn = Button(root, text='Exit', width=20, command=root.destroy)
+exitBtn.pack(side=BOTTOM, pady=50)
+
+
+root.mainloop()
